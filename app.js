ef4cfdb0b730c31461779dc369dd2c6b1d1a1163
Wilson Teh <ywteh13@gmail.com>
2021-05-24 16:11:39 +0800

    Fix stuff in the boxes' stat element
    - Wrap the increased or decreased number stat in a span to be processed by JS
    - Change the statsUpdate's text color and arrow up/down dynamically based on +ve or -ve
    - Remove -ve sign for -ve number

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 let body = document.body
 let toggle = body.querySelector('#switch');
+let statsUpdate = body.querySelectorAll('.box .update span');
 
- 
-// Consoling // 
-console.dir(toggle);
+let limeGreen = 'hsl(163, 72%, 41%)';
+let brightRed = 'hsl(356, 69%, 56%)';
 
-toggle.addEventListener('change', function() {
+// Apply color and arrow of stats update dynamically 
+const applyStatsColor = () => {
+    for (let stat of statsUpdate) {
+        let number = stat.innerText
+        let arrow = stat.previousElementSibling;
+
+        // if number is +ve, apply arrow UP and color GREEN
+        if (number >= 0) {
+            stat.parentElement.style.color = limeGreen;
+            // insert arrow icon
+            arrow.setAttribute('src', './images/icon-up.svg');
+
+        } else if (number < 0) {   // if number is -ve, apply arrow DOWN and color RED
+            stat.parentElement.style.color = brightRed;
+            // insert arrow icon
+            arrow.setAttribute('src', './images/icon-down.svg');
+            // remove '-ve' sign
+            stat.innerHTML = number.replace('-', '');
+        }
+    }
+}
+
+applyStatsColor()
+
+toggle.addEventListener('change', () => {
     body.classList.toggle('dark-theme');
 })
