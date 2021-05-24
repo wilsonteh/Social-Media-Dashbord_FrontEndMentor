51af08de34402af6ec209074215caf9993ef4f10
Wilson Teh <ywteh13@gmail.com>
2021-05-24 15:23:19 +0800

    Apply transition effect when toggling between light theme and dark theme Fix footer color

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ let toggle = body.querySelector('#switch');
 // Consoling // 
 console.dir(toggle);
 
-
 toggle.addEventListener('change', function() {
-    console.log('change')
     body.classList.toggle('dark-theme');
 })
